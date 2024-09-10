874469908bf72f1549570c66a183628f72f9a397
ShivaKumarRagam <ShivaKumarRagam@DESKTOP-H9SL2CT>
2024-09-10 15:01:16 +0530

    OrderServiceCommit

--- EStore.Web/DependencyInjection.cs
+++ EStore.Web/DependencyInjection.cs
@@ -1,16 +1,18 @@
 ﻿using EStore.Application;
+using EStore.Domain;
 using EStore.Infrastructure;
 
 namespace EStore.Web
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddWebDI(this IServiceCollection services)
-        {
-            services.AddApplicationDI()
-                .AddInfrastructureDI();
+        //public static IServiceCollection AddWebDI(this IServiceCollection services, IConfiguration configuration)
+        //{
+        //    services.AddApplicationDI()
+        //        .AddInfrastructureDI()
+        //        .AddDomainDI();
 
-            return services;
-        }
+        //    return services;
+        //}
     }
 }
--- EStore.Web/Program.cs
+++ EStore.Web/Program.cs
@@ -1,4 +1,11 @@
+using EStore.Application.Interfaces;
+using EStore.Application.IRepositories;
+using EStore.Application.Services;
+using EStore.Domain.AutoMapper;
+using EStore.Infrastructure.Data;
+using EStore.Infrastructure.Repositories;
 using EStore.Web;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,8 +15,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddWebDI();
 
+builder.Services.AddDbContext<EStoreDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

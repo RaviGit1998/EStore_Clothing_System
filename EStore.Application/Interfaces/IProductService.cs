d6858e8cb16f14f70bd302d1f115230376d418e3
ShaikNasreen <ShaikNasreen@DESKTOP-RO82TG1>
2024-10-01 11:58:00 +0530

    commit shareViaEmail

--- EStore.Application/Interfaces/IProductService.cs
+++ EStore.Application/Interfaces/IProductService.cs
@@ -18,7 +18,7 @@ namespace EStore.Application.Interfaces
         Task UpdateProductAsync(int productId, UpdateProductDto updateProductDto);
         Task<IEnumerable<ProductDto>> GetProductsByCategoryAsync(int categoryId);
         Task<IEnumerable<ProductVariant>> GetProductVariants();
-        
+        Task<ProductRespDto> GetProductByVariantIdAsync(int productVariantId);
      Task<IEnumerable<ProductDto>> GetFilteredAndSortedProductsAsync(
              int categoryId,
              decimal? minPrice,
